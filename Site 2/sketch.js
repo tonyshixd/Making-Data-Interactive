1598c410e7b1d589aae4756ac8c9f7b9b472cd67
tonyshixd <shixd13@gmail.com>
2020-01-17 12:11:03 -0800

    more files

---
+++ Site 2/sketch.js
@@ -0,0 +1,18 @@
+let myFont;
+function preload() {
+  myFont = loadFont('assets/Nexa.otf');
+}
+
+function setup() {
+  // fill('#ED225D');
+  // textFont(myFont);
+  // textSize(36);
+  // text('p5*js', 10, 50);
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(1000);
+  let anchorGrey = color(237);
+  background(anchorGrey);
+}
