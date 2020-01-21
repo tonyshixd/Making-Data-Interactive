7ec28266d1c5b4f259db45579907902bf863a156
tonyshixd <shixd13@gmail.com>
2020-01-21 15:24:21 -0800

    remove files

--- Site 4/sketch.js
+++ Site 4/sketch.js
@@ -37,21 +37,26 @@ function draw() {
         // now we'll draw a rectangle for each city
         // we need a mathematical way to calculate the position of rectangle
         // because we need to change the position of the rectangle each time we loop (i)
-        rect(120*i, 50, 100, 100);
+        // rect(120*i+50, 50, 100, 100);
 
-        // let density = myRow.getNum("population")/myRow.getNum("area");
+        let density = myRow.getNum("population")/myRow.getNum("area");
+        let densityScale = round(map(density,0,26407,255,0))
 
         // let's add a label
-        text(myRow.getString("city"),120*i,180);
+        fill(255)
+        text(densityScale,120*i,560);
+        text(myRow.getString("city"),120*i,390);
 
         // let's try circles instead of rectangles
         // now we need to change the size of the rectangle to reflect the city size
-        // ellipse(120*i, 300, myRow.getNum("area")/100, myRow.getNum("area")/100);
+        ///ellipse(120*i, 300, myRow.getNum("area"), myRow.getNum("area"));
+        // ellipse(120*i+50, 300, myRow.getNum("area")/100, myRow.getNum("area")/100);
 
         // how about a different calculation for city area
-        // rect(120*i, 440, sqrt(myRow.getNum("area")), sqrt(myRow.getNum("area")));
+
+        fill(densityScale);
+        ellipse(120*i+50, 440, sqrt(myRow.getNum("area")), sqrt(myRow.getNum("area")));
         // text(myRow.getString("city"),120*i,540);
-        // text(density,120*i,560);
 
         // now about about population
         // rect(120*i, 460, sqrt(myRow.getNum("population")), sqrt(myRow.getNum("population")));
