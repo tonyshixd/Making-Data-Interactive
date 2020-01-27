4385a2eed80fe6712ae4e89b73b4bfbd13a2750f
tonyshixd <shixd13@gmail.com>
2020-01-26 23:04:54 -0800

    data recovery

--- Site4/sketch.js
+++ Site4/sketch.js
@@ -1,16 +1,18 @@
-let australiaTB;
-let canadaTB;
-let germany;
-let indiaTB;
-let italy;
-let japanTB;
-let lithuaniaTB;
-let netherlandTB;
-let new_zealandTB;
-let spainTB;
-let united_kingdomTB;
-let united_statesTB;
+// let australiaTB;
+// let canadaTB;
+// let germany;
+// let indiaTB;
+// let italy;
+// let japanTB;
+// let lithuaniaTB;
+// let netherlandTB;
+// let new_zealandTB;
+// let spainTB;
+// let united_kingdomTB;
+// let united_statesTB;
 let master;
+let robotoBlack, robotoReg;
+let input;
 
 
 function preload() {
@@ -28,13 +30,14 @@ function preload() {
   // spainTB = loadTable('/data/spain.csv', 'csv', 'header');
   // united_kingdomTB = loadTable('/data/united_kingdom.csv', 'csv', 'header');
   // united_statesTB = loadTable('/data/united_states.csv', 'csv', 'header');
-  masterTB = loadTable('Making-Data-Interactive/Site4/data/master.csv', 'csv', 'header');
-
+  masterTB = loadTable('data/master.csv', 'csv', 'header');
+  robotoBlack = loadFont('assets/Roboto-Black.ttf');
+  robotoReg = loadFont('assets/Roboto-Regular.ttf');
 
  }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight*2);
     textSize(12);
 }
 
@@ -42,6 +45,8 @@ function draw() {
     noStroke();
     background(50);
     fill(200);
+
+
     // now we're going to create a for loop
     // loops are useful because we can
     for (let i = 0;  i < masterTB.getRowCount(); i++)  {
@@ -53,16 +58,22 @@ function draw() {
         // rect(120*i+50, 50, 100, 100);
 
         let happinessScore = masterTBRow.getNum("Happiness Score");
-        let happinessScale = map(density,5,6.32,0,255);
+        let happinessScale = map(happinessScore,5,6.32,0,255);
 
         // let's add a label
         fill(255)
-        text(happinessScale,120*i,560);
-        text(masterTBRow.getString("Country"),120*i,390);
 
-        fill(densityScale);
-        ellipse(120*i+50, 440, sqrt(masterTBRow.getNum("GDP")), sqrt(masterTBRow.getNum("GDP")));
-        text(myRow.getString("GDP"),120*i,540);
+
+        fill(happinessScale);
+        ellipse(120*i+180, 500, masterTBRow.getNum("GDP")*100, masterTBRow.getNum("GDP")*100);
+        //ellipse(windowWidth/2, windowHeight/2, sqrt(masterTBRow.getNum("GDP")*10000), sqrt(masterTBRow.getNum("GDP")*10000));
+
+
+        fill('#A3A3A3')
+
+        text(masterTBRow.getString("GDP"),125*i+100,600);
+        text(happinessScore,125*i+100,620);
+        text(masterTBRow.getString("Country"),125*i+100,360);
 
         // now about about population
         // rect(120*i, 460, sqrt(myRow.getNum("population")), sqrt(myRow.getNum("population")));
@@ -72,5 +83,19 @@ function draw() {
 
 
     }
+    fill('#FFFFFF');
+    textSize(30);
+    textFont(robotoBlack);
+    text('Data Visualization Experiment',100,160,windowWidth-200);
 
+    fill('#DDDDDD');
+    textSize(12);
+    textFont(robotoReg);
+    text('n October 2016, more than ten thousand people from 104 countries logged how they were feeling over seven days, creating the largest open-source database of emotions. The data is free to download by any individual, organisation, or government to create more effective mental health initiatives. With over 800,000 people suiciding around the world each year, more effective mental health initiatives are desperately needed.',100,210,windowWidth-200);
+
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
